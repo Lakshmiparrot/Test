3a9e37f32da718c6612f9311776e948a06b80b2c
User <User@User-PC>
2021-09-21 13:09:55 +0530

    day string practice

---
+++ Day-3/src/Lab1.java
@@ -0,0 +1,16 @@
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     String d = "Dhanalakshmi";
+     String r = "Ramya";
+     // Finding the Length
+     System.out.println("Sring lenght of first name:" +d.length()+" "+ "String length of second name" +r.length());
+	//compare to
+     System.out.println(r.compareTo(d));
+     //concat 
+     System.out.println(d.concat(" "+r));
+	}
+
+}
